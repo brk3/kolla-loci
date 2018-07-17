d078c42d9e15c658fb91d41ea2ae474b8e8c513f
Paul Bourke <paul.bourke@oracle.com>
2018-07-17 13:04:27 +0100

    Neutron images now working

--- build-all.sh
+++ build-all.sh
@@ -5,13 +5,12 @@ set -ex
 # Images currently published by loci
 #loci=(cinder glance heat horizon ironic keystone neutron nova octavia)
 
-loci=(keystone glance nova)
+loci=(keystone glance nova neutron)
 
 git clone --depth=1 https://git.openstack.org/openstack/kolla
 
 for project in ${loci[@]}; do
-    # TODO(pbourke): filter base images
-    services=$(find kolla/docker/${project} -type d -exec basename {} \;)
+    services=$(find kolla/docker/${project} -type d -exec basename {} \; | grep -v -- -base)
     echo ${services} | xargs -P0 -n1 -t $(dirname $0)/build.sh ${project}
 done
 
--- install.sh
+++ install.sh
@@ -62,19 +62,19 @@ function setup_user {
 function keystone_workarounds {
     if [[ "${SERVICE}" == "keystone" ]]; then
         yum -y install httpd mod_auth_mellon mod_auth_openidc mod_ssl mod_wsgi python-ldappool
-        yum clean all
+        rm -rf /var/cache/yum
         cp ${kolla_scripts}/keystone/keystone/keystone_bootstrap.sh \
             /usr/local/bin/kolla_keystone_bootstrap
         chmod 0755 /usr/local/bin/kolla_keystone_bootstrap
     fi
     if [[ "${SERVICE}" == "keystone-ssh" ]]; then
         yum -y install openssh openssh-server rsync
-        yum clean all
+        rm -rf /var/cache/yum
         chsh --shell /bin/bash keystone
     fi
     if [[ "${SERVICE}" == "keystone-fernet" ]]; then
         yum -y install rsync openssh-clients
-        yum clean all
+        rm -rf /var/cache/yum
         cp ${kolla_scripts}/keystone/keystone-fernet/fetch_fernet_tokens.py /usr/bin/
         cp ${kolla_scripts}/keystone/keystone-fernet/keystone_bootstrap.sh \
             /usr/local/bin/kolla_keystone_bootstrap
@@ -87,11 +87,11 @@ function nova_workarounds {
     sed -i 's|^exec_dirs.*|exec_dirs=/var/lib/kolla/venv/bin,/sbin,/usr/sbin,/bin,/usr/bin,/usr/local/bin,/usr/local/sbin|g' /etc/nova/rootwrap.conf
     if [[ "${SERVICE}" == "nova-ssh" ]]; then
         yum -y install openssh openssh-server
-        yum clean all
+        rm -rf /var/cache/yum
     fi
     if [[ "${SERVICE}" == "nova-placement-api" ]]; then
         yum -y install httpd mod_ssl mod_wsgi
-        yum clean all
+        rm -rf /var/cache/yum
 
         sed -i -r 's,^(Listen 80),#\1,' /etc/httpd/conf/httpd.conf
         sed -i -r 's,^(Listen 443),#\1,' /etc/httpd/conf.d/ssl.conf
@@ -99,11 +99,27 @@ function nova_workarounds {
     if [[ "${SERVICE}" == "nova-compute" ]]; then
         yum -y install http://mirror.centos.org/centos-7/7/extras/x86_64/Packages/centos-release-qemu-ev-1.0-2.el7.noarch.rpm
         yum -y install libvirt-python qemu-img openvswitch bridge-utils
-        yum clean all
+        rm -rf /var/cache/yum
 
         # NOTE(pbourke): needs to be same as kolla's nova-libvirt
         usermod -u 42436 nova
         groupmod -g 42436 nova
+    fi
+}
+
+function neutron_workarounds {
+    if [[ "${SERVICE}" == "neutron-server" ]]; then
+        mkdir /usr/share/neutron
+        cp /etc/neutron/api-paste.ini /usr/share/neutron
+    fi
+    if [[ "${SERVICE}" == "neutron-openvswitch-agent" ]]; then
+        yum -y install openvswitch
+        rm -rf /var/cache/yum
+    fi
+    if [[ "${SERVICE}" == "neutron-l3-agent" || "neutron-dhcp-agent" ]]; then
+        sed -i 's|^exec_dirs.*|exec_dirs=/var/lib/kolla/venv/bin,/sbin,/usr/sbin,/bin,/usr/bin,/usr/local/bin,/usr/local/sbin|g' /etc/neutron/rootwrap.conf
+        yum -y install iproute
+        rm -rf /var/cache/yum
     fi
 }
 
@@ -125,4 +141,8 @@ fi
 
 if [[ "${PROJECT}" == "nova" ]]; then
     nova_workarounds
+fi
+
+if [[ "${PROJECT}" == "neutron" ]]; then
+    neutron_workarounds
 fi
