52f623b0d9d6ac970efc1e50cc1de2c7d0747cd8
Paul Bourke <paul.bourke@oracle.com>
2018-07-26 20:31:21 +0100

    Start working on infra services in build again

--- build.sh
+++ build.sh
@@ -17,6 +17,14 @@ build_loci() {
     #git pull https://git.openstack.org/openstack/loci refs/changes/55/583255/7
     pushd /root/loci
 
+    case "${project}" in
+        rabbitmq|mariadb)
+            local loci_project="infra"
+            ;;
+        *)
+            local loci_project="${project}"
+    esac
+
     case "${KOLLA_BASE_DISTRO}" in
         centos)
             docker build dockerfiles/centos/ \
@@ -44,7 +52,7 @@ build_loci() {
         --build-arg http_proxy=$http_proxy \
         --build-arg https_proxy=$https_proxy \
         --build-arg no_proxy=$no_proxy \
-        --build-arg PROJECT=${project} \
+        --build-arg PROJECT=${loci_project} \
         --build-arg PROFILES="kolla ${project}" \
         --build-arg FROM=${from} \
         --build-arg WHEELS="loci/requirements:${TAG}-${KOLLA_BASE_DISTRO}" \
@@ -95,7 +103,7 @@ fi
 
 # Build the loci base images using the kolla profile
 echo ${PROJECTS[@]} | xargs --max-procs=0 --delimiter=' ' --max-args=1 --verbose -I {} \
-    bash -c "build_loci \$@" _ {}
+    bash -e -x -c "build_loci \$@" _ {}
 
 # Build the kolla-loci images
 git clone --depth=1 https://git.openstack.org/openstack/kolla ${TMPDIR}/kolla
@@ -103,7 +111,7 @@ for project in ${PROJECTS[@]}; do
     services=$(find ${TMPDIR}/kolla/docker/${project} -type d -exec basename {} \; | sort --unique \
         | grep -v -- -base)
     echo ${services} | xargs --max-procs=0 --delimiter=' ' --max-args=1 --verbose -I {} \
-        bash -c "build_kolla_loci ${project} \$@" _ {}
+        bash -e -x -c "build_kolla_loci ${project} \$@" _ {}
 done
 
 rm -rf ${TMPDIR}
